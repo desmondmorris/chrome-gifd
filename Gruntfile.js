dc124c80d690fc0aa29671a7f917f2f33d70b60e
Desmond Morris <hi@desmondmorris.com>
2013-11-16 18:10:07 -0500

    Adds grunt tasks that generates package zip file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,26 @@
 'use strict';
 module.exports = function(grunt) {
-  grunt.initConfig({
-    mocha: {
-      all: ['test/index.html']
-    }
-  });
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.registerTask('default', ['mocha']);
+    grunt.initConfig({
+        mocha: {
+            all: ['test/index.html']
+        },
+        clean: ["package"],
+        compress: {
+            dist: {
+                options: {
+                    archive: 'package/gifd.zip'
+                },
+                files: [{
+                    expand: true,
+                    cwd: '',
+                    src: ['**'],
+                    dest: ''
+                }]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['mocha', 'clean', 'compress:dist']);
 };
