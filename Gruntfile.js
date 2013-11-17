a9aac5189345019ad870e0c509a33cf9004e108a
Desmond Morris <hi@desmondmorris.com>
2013-11-16 20:31:08 -0500

    Release 1.0.2 - Fixes compression grunt task to reduce files included in package

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,23 +4,33 @@ module.exports = function(grunt) {
         mocha: {
             all: ['test/index.html']
         },
-        clean: ["package"],
+        clean: [
+            "dist",
+            "package"
+        ],
+        copy: {
+          dist: {
+            files: [
+              // includes files within path
+              {expand: true, src: ['src/*'], dest: 'dist/', filter: 'isFile'},
+              {expand: true, src: ['components/**'], dest: 'dist/', filter: 'isFile'},
+              {expand: true, src: ['manifest.json'], dest: 'dist/', filter: 'isFile'},
+              {expand: true, src: ['icon*.png'], dest: 'dist/', filter: 'isFile'}
+            ]
+          }
+        },
         compress: {
             dist: {
                 options: {
                     archive: 'package/gifd.zip'
                 },
-                files: [{
-                    expand: true,
-                    cwd: '',
-                    src: ['**'],
-                    dest: ''
-                }]
+                files: [{src: ['dist/**'], dest: ''}]
             }
         }
     });
     grunt.loadNpmTasks('grunt-mocha');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.registerTask('default', ['mocha', 'clean', 'compress:dist']);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['mocha', 'clean', 'copy', 'compress']);
 };
